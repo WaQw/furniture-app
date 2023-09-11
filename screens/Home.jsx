02f8c7ae6648f0ae7aef03aba10992a64ef152b5
WaQw <47028829+WaQw@users.noreply.github.com>
2023-09-11 00:01:51 -0400

    Home: WelcomeTxts & SearchBar

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,8 +1,9 @@
-import { TouchableOpacity, Text, View } from 'react-native'
+import { TouchableOpacity, Text, View, ScrollView } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { Ionicons, Fontisto } from '@expo/vector-icons'
 import styles from './home.style'
+import { Welcome } from '../components'
 
 const Home = () => {
     return (
@@ -21,9 +22,12 @@ const Home = () => {
                             <Fontisto name='shopping-bag' size={24}/>
                         </TouchableOpacity>
                     </View>
-
-                </View>
+                </View>   
             </View>
+
+            <ScrollView>
+                <Welcome/>
+            </ScrollView>
         </SafeAreaView>
     )
 }
