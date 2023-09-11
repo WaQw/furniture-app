99e3308148b1ea683ef2c079f38dc1216faf72c8
WaQw <47028829+WaQw@users.noreply.github.com>
2023-09-11 13:59:05 -0400

    Home: Modify Margin of ProductRow

--- components/products/ProductRow.jsx
+++ components/products/ProductRow.jsx
@@ -2,6 +2,7 @@ import { FlatList, Text, View } from 'react-native'
 import React from 'react'
 import { SIZES } from '../../constants'
 import ProductCardView from './ProductCardView'
+import styles from './productRow.style'
 
 const ProductRow = () => {
     
@@ -9,7 +10,7 @@ const ProductRow = () => {
 
 
     return (
-        <View style={{marginTop: SIZES.medium}}>
+        <View style={styles.container}>
             <FlatList 
             data={products}
             renderItem={({ item }) => <ProductCardView/>}
