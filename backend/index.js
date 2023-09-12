42cb5cfca0f53e605cc9fca3020606ba242c5b1c
WaQw <47028829+WaQw@users.noreply.github.com>
2023-09-12 16:44:48 -0400

    Configure to Connect to DB

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const dotenv = require("dotenv")
+const mongoose = require("mongoose")
 const app = express()
 const port = 3000
+
+dotenv.config()
+mongoose.connect(process.env.MONGO_URL).then(() => console.log("db connected")).catch((err) => console.log(err))
+
 
 app.get('/', (req, res) => res.send('Furniture World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
