1bafae1161fcd5a437259b2c3240da1c8f1a80c9
WaQw <47028829+WaQw@users.noreply.github.com>
2023-09-13 11:45:20 -0400

    Pass Data to ProductRow & ProductCard

--- frontend/components/products/ProductCardView.jsx
+++ frontend/components/products/ProductCardView.jsx
@@ -1,27 +1,25 @@
 import { TouchableOpacity, Text, View, Image } from 'react-native'
 import React from 'react'
 import styles from './productCardView.style'
-import p1 from "../../assets/images/fn1.jpg"
 import { Ionicons } from '@expo/vector-icons'
 import { COLORS } from "../../constants"
 import { useNavigation } from '@react-navigation/native'
 
-const ProductCardView = () => {
+const ProductCardView = ({item}) => {
 
-    const DEFAULT_IMAGE = Image.resolveAssetSource(p1).uri;
     const navigation = useNavigation();
 
     return (
         <TouchableOpacity onPress={() => navigation.navigate("ProductDetails")}>
            <View style={styles.container}>
                 <View style={styles.imageContainer}>
-                    <Image source={{uri: DEFAULT_IMAGE}} style={styles.image} />
+                    <Image source={{uri: item.imageUrl}} style={styles.image} />
                 </View>
 
                 <View style={styles.details}>
-                    <Text style={styles.title} numberOfLines={1}>Product</Text>
-                    <Text style={styles.supplier} numberOfLines={1}>AnAn Company</Text>
-                    <Text style={styles.price}>$3000</Text>
+                    <Text style={styles.title} numberOfLines={1}>{item.title}</Text>
+                    <Text style={styles.supplier} numberOfLines={1}>{item.supplier}</Text>
+                    <Text style={styles.price}>${item.price}</Text>
                 </View>
 
                 <TouchableOpacity style={styles.addBtn}>
