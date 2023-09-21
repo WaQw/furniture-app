fa14058139c7140c87741570e1bd1d57812d8e85
WaQw <47028829+WaQw@users.noreply.github.com>
2023-09-21 14:33:23 -0400

    Profile: Set up

--- frontend/screens/Profile.jsx
+++ frontend/screens/Profile.jsx
@@ -1,15 +1,46 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { Image, Text, TouchableOpacity, View } from 'react-native'
+import React, {useState, useEffect} from 'react'
+import styles from './profile.style'
+import { StatusBar } from 'expo-status-bar'
+import { COLORS, SIZES} from "../constants"
 
 const Profile = () => {
+
+    const [userData, setUserData] = useState(null)
+    const [userLogin, setUserLogin] = useState(false)
+
     return (
-        <SafeAreaView>
-            <Text>Profile</Text>
-        </SafeAreaView>
+        <View style={styles.container}>
+            <View style={styles.container}>
+                <StatusBar backgroundColor={COLORS.gray}/>
+                <View style={{width: '100%'}}>
+                    <Image 
+                        source={require('../assets/images/space.jpg')}
+                        style={styles.cover}
+                    />
+                </View>
+                <View style={styles.profileContainer}>
+                    <Image 
+                        source={require('../assets/images/profile.jpeg')}
+                        style={styles.profile}
+                    />
+                    <Text style={styles.name}>
+                        {userLogin === true ? userData.name : "Please login into your account"}
+                    </Text>
+                    {userLogin === false ? (
+                        <TouchableOpacity>
+                            <View style={styles.loginBtn}>
+                                <Text style={styles.menuText}>L O G I N    </Text>
+                            </View>
+                        </TouchableOpacity>
+                    ) : (<View style={styles.loginBtn}>
+                            <Text style={styles.menuText}>angela@outlook.com    </Text>
+                        </View>
+                    ) }
+                </View>
+            </View>
+        </View>
     )
 }
 
 export default Profile
-
-const styles = StyleSheet.create({})
