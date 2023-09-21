8b1821ff4a848d0268e3fce4093582ccaaf9010d
WaQw <47028829+WaQw@users.noreply.github.com>
2023-09-21 00:59:22 -0400

    Backend: Add Order Controller and Router

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ const productRouter = require('./routes/products')
 const authRouter = require('./routes/auth')
 const userRouter = require('./routes/user')
 const cartRouter = require('./routes/cart')
+const orderRouter = require('./routes/order')
 const port = 3000
 
 dotenv.config()
@@ -18,5 +19,6 @@ app.use('/api/products', productRouter)
 app.use('/api/', authRouter)
 app.use('/api/users', userRouter)
 app.use('/api/carts', cartRouter)
+app.use('/api/orders', orderRouter)
 
 app.listen(process.env.PORT || port, () => console.log(`Example app listening on port ${process.env.PORT}!`))
