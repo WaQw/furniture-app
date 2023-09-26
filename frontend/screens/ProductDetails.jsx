9704c14ed2169b8c727a71c6d1da7d2f181b5153
WaQw <47028829+WaQw@users.noreply.github.com>
2023-09-26 13:05:49 -0400

    ProductDetail: Check User Login

--- frontend/screens/ProductDetails.jsx
+++ frontend/screens/ProductDetails.jsx
@@ -1,17 +1,19 @@
 import { TouchableOpacity, View, Image, Text } from 'react-native'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useRoute } from '@react-navigation/native'
 import { Ionicons, SimpleLineIcons, MaterialCommunityIcons, Fontisto } from '@expo/vector-icons'
 import { COLORS, SIZES} from "../constants"
 import styles from './productDetails.style'
 import AddToCart from '../hook/AddToCart'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const ProductDetails = ({navigation}) => {
 
     const route = useRoute();
     const {item} = route.params;
 
-    const [count, setCount] = useState(1)
+    const [count, setCount] = useState(1);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
     const increment = () => {
         setCount(count + 1)
@@ -23,6 +25,49 @@ const ProductDetails = ({navigation}) => {
         }
     }
 
+    useEffect(() => {
+        checkUser();
+    },[]);
+
+    const checkUser = async() => {
+        try {
+            const id = AsyncStorage.getItem('id');
+            if(id !== null) {
+                setIsLoggedIn(true)
+                console.log('user logged in')
+            } else {
+                console.log('user not logged in')
+            }
+        } catch (error) {
+            
+        }
+    }
+
+    const handlePress = () => {
+        console.log(isLoggedIn)
+        if(isLoggedIn === false) {
+            navigation.navigate('Login')
+        } else {
+        
+        }
+    }
+
+    const handleBuy = () => {
+        if(isLoggedIn === false) {
+            navigation.navigate('Login')
+        } else {
+            
+        }
+    }
+
+    const handleCart = () => {
+        if(isLoggedIn === false) {
+            navigation.navigate('Login')
+        } else {
+            AddToCart(item._id, count)
+        }
+    }
+
     return (
         <View style={styles.container}>
             <View style={styles.upperRow}>
@@ -30,7 +75,7 @@ const ProductDetails = ({navigation}) => {
                     <Ionicons name='chevron-back-circle' size={30}/>
                 </TouchableOpacity>
 
-                <TouchableOpacity onPress={() => {}}>
+                <TouchableOpacity onPress={() => handlePress()}>
                     <Ionicons name='heart' size={30} color={COLORS.primary} />
                 </TouchableOpacity>
             </View>
@@ -87,11 +132,11 @@ const ProductDetails = ({navigation}) => {
                 </View>
 
                 <View style={styles.cartRow}>
-                    <TouchableOpacity onPress={() => {}} style={styles.cartBtn}>
+                    <TouchableOpacity onPress={() => handleBuy()} style={styles.cartBtn}>
                         <Text style={styles.cartTitle}>BUY NOW</Text>
                     </TouchableOpacity>
 
-                    <TouchableOpacity onPress={() => AddToCart(item._id, count)} style={styles.addCart}>
+                    <TouchableOpacity onPress={() => handleCart()} style={styles.addCart}>
                         <Fontisto name='shopping-bag' size={22} color={COLORS.lightWhite} />
                     </TouchableOpacity>
                 </View>
