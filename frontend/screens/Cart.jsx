619871319c7a5d9decc3bdec9a0724c91ecc92ef
WaQw <47028829+WaQw@users.noreply.github.com>
2023-10-06 20:49:36 -0400

    Profile: Add Cart Basic Layout

--- frontend/screens/Cart.jsx
+++ frontend/screens/Cart.jsx
@@ -1,15 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { TouchableOpacity, Text, View } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import styles from './cart.style'
+import { Ionicons, SimpleLineIcons } from '@expo/vector-icons'
+import { COLORS } from '../constants'
 
-const Cart = () => {
+const Cart = ({navigation}) => {
     return (
-        <SafeAreaView>
-            <Text>Cart</Text>
+        <SafeAreaView style={styles.container}>
+            <View style={styles.titleRow}>
+                <TouchableOpacity onPress={() => navigation.goBack()}>
+                    <Ionicons
+                        name='chevron-back-circle'
+                        size={30}
+                        color={COLORS.primary}
+                    />
+                </TouchableOpacity>
+
+                <Text style={styles.titleText}>Cart</Text>
+            </View>
         </SafeAreaView>
     )
 }
 
 export default Cart
-
-const styles = StyleSheet.create({})
---
+++ frontend/screens/cart.style.js
@@ -0,0 +1,47 @@
+import { StyleSheet } from "react-native"
+import { SIZES, COLORS } from '../constants'
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 20,
+        marginHorizontal: 20
+    },
+    titleRow: {
+        flexDirection: "row",
+        justifyContent: "flex-start",
+        alignItems: "center",
+        width: SIZES.width - 50,
+        marginBottom: 12
+    },
+    titleText: {
+        fontFamily: "bold",
+        fontSize: SIZES.xLarge,
+        letterSpacing:2,
+        marginLeft: SIZES.small
+    },
+    favContainer: {
+        flex: 1,
+        justifyContent: "space-between",
+        alignItems: "center",
+        flexDirection: "row",
+        marginBottom: SIZES.xSmall,
+        padding: SIZES.medium,
+        borderRadius: SIZES.small,
+        backgroundColor: "#FFF",
+        shadowColor: COLORS.secondary
+    },
+    imageContainer: {
+        width: 70,
+        borderRadius: SIZES.medium,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    image: {
+        width: "100%",
+        height: 65,
+        borderRadius: SIZES.small,
+        resizeMode: "cover"
+    },
+})
+
+export default styles
